1be97dfaed62c56aed8d56f9a24e339694bcf18f
Vincent Durand <vincent.durand@madwork.org>
2013-11-18 19:23:04 +0100

    Initial commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+set :css_dir,     'stylesheets'
+set :js_dir,      'javascripts'
+set :images_dir,  'images'
+
+set :relative_links, true
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+  activate :gzip
+end
